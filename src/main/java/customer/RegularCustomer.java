74c7a83bb0e32a920bf1e37e46daeb64f61bd59a
Robert Pauls <rpauls2002@gmail.com>
2023-04-04 11:15:17 +0100

    Add Customer and DiscountPlan entities

---
+++ src/main/java/customer/RegularCustomer.java
@@ -0,0 +1,24 @@
+package customer;
+
+public class RegularCustomer extends Customer {
+
+	private boolean payLater;
+
+	public RegularCustomer(String username, String forename, String surname, String address, String postcode, String phone, String email, String status, boolean payLater) {
+		super(username, forename, surname, address, postcode, phone, email, status);
+		this.payLater = payLater;
+	}
+
+	public boolean getPayLater() {
+		return this.payLater;
+	}
+
+	/**
+	 * 
+	 * @param payLater
+	 */
+	public void setPayLater(boolean payLater) {
+		this.payLater = payLater;
+	}
+
+}
